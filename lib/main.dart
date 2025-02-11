72205914a9291db9b97f22834c9dae0b55b0d6a2
DotBox <aadikeshu2305@gmail.com>
2025-02-11 22:17:51 +0530

    fixed merge conflicts with main branch

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Weave',
-      home: Scaffold(
-        backgroundColor: Colors.black,
-      ),
-    );
+    return const MaterialApp();
   }
 }
