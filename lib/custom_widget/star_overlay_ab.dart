9b151ded00bf3cf37f5ad2ab9150563c3bd48ead
Abinash Mohanty <abinashmohanty8059@gmail.com>
2025-02-12 20:07:27 +0530

    added comments to staroverlay page

--- lib/custom_widget/star_overlay_ab.dart
+++ lib/custom_widget/star_overlay_ab.dart
@@ -1,15 +1,15 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; // Import Flutter's material design package.
 
 class StarOverlay extends StatelessWidget {
-  const StarOverlay({super.key});
+  const StarOverlay({super.key}); // Constructor for the widget.
 
   @override
   Widget build(BuildContext context) {
-    return Positioned.fill(
+    return Positioned.fill( // will fill the while screen with stars
       child: Image.asset(
-        "ase/star.png", // Ensure this path is correct
-        fit: BoxFit.cover,
-        opacity: const AlwaysStoppedAnimation(1),
+        "ase/star.png", // the name of my assets folder is ase
+        fit: BoxFit.cover, // no distortion will be there
+        opacity: const AlwaysStoppedAnimation(1), // 1 means full opacity.
       ),
     );
   }
