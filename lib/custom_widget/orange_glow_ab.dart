fcf8b7cdcdb8d371021a3d52f814d3613b2c129f
Abinash Mohanty <abinashmohanty8059@gmail.com>
2025-02-12 19:58:43 +0530

    added comments on the glow page

--- lib/custom_widget/orange_glow_ab.dart
+++ lib/custom_widget/orange_glow_ab.dart
@@ -1,24 +1,25 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; 
 
 class OrangeGlow extends StatelessWidget {
-  final double top;
-  final double left;
+  final double top; 
+  final double left; 
 
+  // Constructor to initialize postion of top and left
   const OrangeGlow({super.key, required this.top, required this.left});
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      top: top,
-      left: left,
+    return Positioned( // Positions the glow effect on each page
+      top: top, 
+      left: left, 
       child: Container(
-        width: 300,
-        height: 300,
+        width: 300, 
+        height: 300, 
         decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          gradient: RadialGradient(
-            colors: [
-              const Color(0xFFE76F04).withAlpha(80),
+          shape: BoxShape.circle, 
+          gradient: RadialGradient( // Creates a radial gradient effect
+            colors: [ // Defines different shades of orange with varying opacity.
+              const Color(0xFFE76F04).withAlpha(80), // Strongest glow.
               const Color(0xFFE76F04).withAlpha(50),
               const Color(0xFFE76F04).withAlpha(45),
               const Color(0xFFE76F04).withAlpha(28),
@@ -28,11 +29,13 @@ class OrangeGlow extends StatelessWidget {
               const Color(0xFFE76F04).withAlpha(10),
               const Color(0xFFE76F04).withAlpha(8),
               const Color(0xFFE76F04).withAlpha(5),
-              const Color(0xFFE76F04).withAlpha(3),
+              const Color(0xFFE76F04).withAlpha(3), // Faintest glow.
             ],
-            stops: const [0.0, 0.2, 0.3, 0.4, 0.45, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
-            radius: 1.0,
-            center: Alignment.center,
+            stops: const [ //colour change position
+              0.0, 0.2, 0.3, 0.4, 0.45, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0
+            ],
+            radius: 1.0, // Controls the spread of the radial gradient.
+            center: Alignment.center, // Centers the glow effect.
           ),
         ),
       ),
